190e272bf8eeb7d035d68797f307fda327695b80
Ishika <ishika02908@gmail.com>
2024-07-08 12:18:54 +0530

    banner

--- banner.py
+++ banner.py
@@ -6,6 +6,15 @@ def printBanner():
       _|   \___/  _|    \__| _| ____/  \___| \__| \___| \___| \__| 
                                                  
     """
+    copyright_info = """
+-------------------------------------------------
+FortiDetect - An Intrusion Detection Systen
+Created by: Ishika Srivastava
+GitHub : https://github.com/ishika-srivastava/FortiDetect-An-Intrusion-Detection-System
+Version: 1.0
+License: MIT
+(c) 2024 Ishika Srivastava. All rights reserved.
+-------------------------------------------------
+"""
     print(banner)
-
-printBanner()
+    print(copyright_info)
