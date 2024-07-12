6414b2c5be89da35df721f1a7796a4d5719665ff
Ishika Srivastava <102409555+ishika-srivastava@users.noreply.github.com>
2024-07-12 23:39:26 +0530

    Update help.py

--- help.py
+++ help.py
@@ -4,14 +4,14 @@ def helpMenu():
 Usage: 
 
 Arguments:
--h, --help: show the help message and exit
---start: start capturing network packets
---stop: stop capturing network packets
---logfile <PATH>: specify the file path where intrusion logs will be saved
---interface <INTERFACE>: specify the network interface to capture packets from
---threshold <VALUE>: set the threshold for triggering alerts based on suspicious activity
---model <PATH>: specify the path to the trained machine learning model
---features <METHOD>: choose  the method for extracting features from packets for anomaly detection
---viewlogs: display the intrusion logs
---dashboardport <PORT>: specify the port for web dashboard
+-h, --help              : show the help message and exit
+--start                 : start capturing network packets
+--stop                  : stop capturing network packets
+--logfile <PATH>        : specify the file path where intrusion logs will be saved
+--interface <INTERFACE> : specify the network interface to capture packets from
+--threshold <VALUE>     : set the threshold for triggering alerts based on suspicious activity
+--model <PATH>          : specify the path to the trained machine learning model
+--features <METHOD>     : choose  the method for extracting features from packets for anomaly detection
+--viewlogs              : display the intrusion logs
+--dashboardport <PORT>  : specify the port for web dashboard
 """
